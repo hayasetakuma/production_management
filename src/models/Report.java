2ee0ae5c53f4038495a60eff9751e7c1ebb3d345
taro.kirameki <taro.kirameki@techacademy.jp>
2024-03-19 17:34:24 +0900

    getAllReportsを追加

--- src/models/Report.java
+++ src/models/Report.java
@@ -7,10 +7,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "messages")
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllReports",
+        query = "SELECT r FROM Report AS r ORDER BY r.id DESC"
+    )
+})
+@Table(name = "reports")
 public class Report {
     @Id
     @Column(name = "id")
