fee4c9ecbb4355ae94f99e60fa60d6bccefe82df
taro.kirameki <taro.kirameki@techacademy.jp>
2024-03-23 11:52:23 +0900

    UpdateServlet.javaを作成

---
+++ src/controllers/UpdateServlet.java
@@ -0,0 +1,67 @@
+package controllers;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Report;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class UpdateServlet
+ */
+@WebServlet("/update")
+public class UpdateServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UpdateServlet() {
+        super();
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String _token = request.getParameter("_token");
+        if(_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
+
+            // セッションスコープからメッセージのIDを取得して
+            // 該当のIDのメッセージ1件のみをデータベースから取得
+            Report r = em.find(Report.class, (Integer)(request.getSession().getAttribute("report_id")));
+
+            // フォームの内容を各フィールドに上書き
+            String product_name = request.getParameter("product_name");
+            r.setProduct_name(product_name);
+
+            String product_number = request.getParameter("product_number");
+            r.setProduct_number(product_number);
+
+            String defective_product = request.getParameter("defective_product");
+            r.setDefective_product(defective_product);
+
+            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+            r.setUpdated_at(currentTime);       // 更新日時のみ上書き
+
+            // データベースを更新
+            em.getTransaction().begin();
+            em.getTransaction().commit();
+            em.close();
+
+            // セッションスコープ上の不要になったデータを削除
+            request.getSession().removeAttribute("report_id");
+
+            // indexページへリダイレクト
+            response.sendRedirect(request.getContextPath() + "/index");
+        }
+    }
+}
